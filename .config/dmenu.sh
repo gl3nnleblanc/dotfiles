20e891cfcd68849fb6cef96b20a77aa7a5c8f756
Glenn LeBlanc <g.leblanc135@gmail.com>
2015-10-23 05:50:02 -0700

    Added dmenu.sh

---
+++ .config/dmenu.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#dmenu_run -x 45 -y 1060 -w 1830 -h 20 -i -p "launch:" -fn "-xos4-terminus-medium-r-*-*-12-*" -nb "#262626" -nf "#a0a0a0" -sb "#262626" -sf "#3b3b4b"
+rofi -show run -terminal urxvt -lines 7 -eh 2 -width 100 -padding 800 -opacity "85" -bw 0 -bc "#101010" -bg "#101010" -fg "#b1b1b1"-hlbg "#000000" -hlfg "#fafafa" -font "Terminus 8"
